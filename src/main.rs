3d89ddcda3c8ab94d07b77c461c473e69004b058
Soomin Kimu <soominkimu@gmail.com>
2019-01-29 23:56:40 +0900

    chap.5 Structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+    width : u32,
+    height: u32,
+}
+
+impl Rectangle {  // method
+    fn area(&self) -> u32 {  // immutable borrow
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width  > other.width &&
+        self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {  // associated function
+        Rectangle { width: size, height: size }
+    }
+}
+
 fn main() {
     let a = [10, 20, 30, 40, 50];
 
@@ -19,11 +40,27 @@ fn main() {
     println!("word = {}", word);
 
     let t2 = String::from("hello world");
-    let word2 = first_word2(&t2[..]);
+    let _word2 = first_word2(&t2[..]);
     let t3 = "hello world";
     let word2 = first_word2(t3);
     //t2.clear();
     println!("word2 = {}", word2);
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+    println!("rect1 is {:?}",  rect1);
+    println!("rect1 is {:#?}", rect1);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("rect sq {:?}", Rectangle::square(3));
 }
 
 fn change(some_string: &mut String) {
