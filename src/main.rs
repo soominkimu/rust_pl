e9e86052a1a77a4d92e6fcb17e36997fb76f782d
Soomin Kimu <soominkimu@gmail.com>
2019-02-05 18:05:58 +0900

    chap.8 working on

--- src/main.rs
+++ src/main.rs
@@ -97,6 +97,44 @@ fn main() {
     if let Some(3) = some_u8_value {
         println!("Three!");
     }
+
+    println!("Value = {}", value_in_cents(Coin::Penny));
+    println!("Value = {}", value_in_cents(Coin::Nickel));
+    println!("Value = {}", value_in_cents(Coin::Dime));
+    println!("Value = {}", value_in_cents(Coin::Quarter));
+
+    // Chap.8 Common Collections
+    let mut v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None        => println!("There is no third element."),
+    }
+
+    let first = &v[0];
+    println!("The first element is {}", first);
+    v.push(6);
+
+    for i in &mut v {
+        *i += 10;
+        println!("{}", i);
+    }
+
+#[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    println!("SpreadsheetCell row = {:?}", row);
 }
 
 fn change(some_string: &mut String) {
@@ -126,3 +164,5 @@ fn first_word2(s: &str) -> &str {
 
     &s[..]
 }
+
+
