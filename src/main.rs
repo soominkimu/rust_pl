36b0820055a06a9c340034c444917a136417a9f3
Soomin Kimu <soominkimu@gmail.com>
2019-04-13 22:18:31 +0900

    chapters separated as fn

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,53 @@
-#[derive(Debug)]
-struct Rectangle {
-    width : u32,
-    height: u32,
+const C_RED    : &str = "\x1B[0;31m";
+const C_GREEN  : &str = "\x1B[0;32m";
+const C_YELLOW : &str = "\x1B[0;33m";
+const C_BLUE   : &str = "\x1B[0;34m";
+const C_END    : &str = "\x1B[0m";
+
+fn print_chap(n: i32, s: &str) {
+    println!("{}*** Chap.{}{}{} {}{}", C_BLUE, C_YELLOW, n, C_GREEN, s, C_END);
 }
 
-impl Rectangle {  // method
-    fn area(&self) -> u32 {  // immutable borrow
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width  > other.width &&
-        self.height > other.height
-    }
-
-    fn square(size: u32) -> Rectangle {  // associated function
-        Rectangle { width: size, height: size }
-    }
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
-
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny   => 1,
-        Coin::Nickel  => 5,
-        Coin::Dime    => 10,
-        Coin::Quarter => 25,
-    }
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None    => None,
-        Some(i) => Some(i+1),
-    }
-}
-
-fn main() {
+fn chap03() {
+    print_chap(3, "Common Programming Concepts");
+    // Listing 3-5: Looping through each element of a collection using a for loop
     let a = [10, 20, 30, 40, 50];
 
+    print!("the value is: ");
     for element in a.iter() {
-        println!("the value is: {}", element);
+        print!("{},", element);
+    }
+    println!();
+}
+
+fn chap04() {
+    print_chap(4, "Understanding Ownership");
+    fn change(some_string: &mut String) {
+        some_string.push_str(", world");
+    }
+
+    fn first_word(s: &String) -> usize {
+        let bytes = s.as_bytes();
+
+        for (i, &item) in bytes.iter().enumerate() {  // enumerate method returns a tuple, destructure it
+            if item == b' ' {
+                return i;
+            }
+        }
+
+        s.len()
+    }
+
+    fn first_word2(s: &str) -> &str {
+        let bytes = s.as_bytes();
+
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return &s[0..i];
+            }
+        }
+
+        &s[..]
     }
 
     let s1 = String::from("hello");
@@ -68,6 +69,31 @@ fn main() {
     let word2 = first_word2(t3);
     //t2.clear();
     println!("word2 = {}", word2);
+}
+
+fn chap05() {
+    print_chap(5, "Using Structs to Structure Related Data");
+
+    #[derive(Debug)]
+    struct Rectangle {
+        width : u32,
+        height: u32,
+    }
+
+    impl Rectangle {  // method
+        fn area(&self) -> u32 {  // immutable borrow
+            self.width * self.height
+        }
+
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width  > other.width &&
+            self.height > other.height
+        }
+
+        fn square(size: u32) -> Rectangle {  // associated function
+            Rectangle { width: size, height: size }
+        }
+    }
 
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
@@ -84,6 +110,33 @@ fn main() {
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
     println!("rect sq {:?}", Rectangle::square(3));
+}
+
+fn chap06() {
+    print_chap(6, "Enums and Pattern Matching");
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny   => 1,
+            Coin::Nickel  => 5,
+            Coin::Dime    => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None    => None,
+            Some(i) => Some(i+1),
+        }
+    }
 
     let five = Some(5);
     println!("plus_one(five) = {:?}", plus_one(five).unwrap());
@@ -98,12 +151,16 @@ fn main() {
         println!("Three!");
     }
 
-    println!("Value = {}", value_in_cents(Coin::Penny));
-    println!("Value = {}", value_in_cents(Coin::Nickel));
-    println!("Value = {}", value_in_cents(Coin::Dime));
-    println!("Value = {}", value_in_cents(Coin::Quarter));
+    println!("Value = {},{},{},{}",
+        value_in_cents(Coin::Penny),
+        value_in_cents(Coin::Nickel),
+        value_in_cents(Coin::Dime),
+        value_in_cents(Coin::Quarter));
+}
 
-    // Chap.8 Common Collections
+fn chap08() {
+    print_chap(8, "Common Collections");
+
     let mut v = vec![1, 2, 3, 4, 5];
     let third: &i32 = &v[2];
     println!("The third element is {}", third);
@@ -119,10 +176,11 @@ fn main() {
 
     for i in &mut v {
         *i += 10;
-        println!("{}", i);
+        print!("{},", i);
     }
+    println!();
 
-#[derive(Debug)]
+    #[derive(Debug)]
     enum SpreadsheetCell {
         Int(i32),
         Float(f64),
@@ -187,65 +245,149 @@ fn main() {
       *count += 1;
     }
     println!("{:?}", map1);
+}
 
+fn chap09() {
+    print_chap(9, "Error Handling");
     //let v_panic = vec![1,2,3];
     //v_panic[99];  // to panic
     // run $ RUST_BACKTRACE=1 cargo run
 
     use std::fs::File;
     use std::io::ErrorKind;
-    /*
 
-    let f = File::open("hello.txt");
+    use std::io;
+    use std::io::Read;
 
-    let f = match f {
+    const HELLO_TXT: &str = "hello.txt";
+
+    // Propagating Errors, Listing 9-6
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open(HELLO_TXT);
+
+        let mut f = match f {
+          Ok(file) => file,
+          Err(e)   => return Err(e),
+        };
+
+        let mut s = String::new();
+
+        match f.read_to_string(&mut s) {
+          Ok(_)  => Ok(s),
+          Err(e) => Err(e)
+        }
+    }
+
+    fn read_username_from_file_short() -> Result<String, io::Error> {
+        let mut f = File::open(HELLO_TXT)?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
+
+    fn read_username_from_file_chained() -> Result<String, io::Error> {
+        let mut s = String::new();
+
+        File::open(HELLO_TXT)?.read_to_string(&mut s)?;
+
+        Ok(s)
+    }
+
+    use std::fs;
+
+    fn read_username_from_file_shortest() -> Result<String, io::Error> {
+        fs::read_to_string(HELLO_TXT)
+    }
+
+    // Listing 9-10: A Guess type that will only continue with values between 1 and 100
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+
+            Guess {
+                value
+            }
+        }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+
+    let _f1 = File::open(HELLO_TXT);
+
+    let _f1 = match _f1 {
         Ok(file) => file,
         Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
+            ErrorKind::NotFound => match File::create(HELLO_TXT) {
                 Ok(fc) => fc,
                 Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
             },
             other_error => panic!("There was a problem opening the file: {:?}", other_error),
         },
     };
-    */
 
-    let f = File::open("hello.txt").map_err(|error| {
+    let _f = File::open(HELLO_TXT).map_err(|error| {
         if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
+            File::create(HELLO_TXT).unwrap_or_else(|error| {
                 panic!("Tried to create file but there was a problem: {:?}", error);
             })
         } else {
             panic!("There was a problem opening the file: {:?}", error);
         }
     });
-}  // main
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    // Shortcuts for Panic on Error: unwrap and expect
+    //let _f2 = File::open("hello.txt").unwrap();
+    let _f2 = File::open(HELLO_TXT).expect("Filed to open hello.txt");
+
+    println!("{}", read_username_from_file().unwrap());
+    println!("{}", read_username_from_file_short().unwrap());
+    println!("{}", read_username_from_file_chained().unwrap());
+    println!("{}", read_username_from_file_shortest().unwrap());
+
+    let n910 = Guess::new(50);
+    println!("Guess: {}", n910.value);
 }
 
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {  // enumerate method returns a tuple, destructure it
-        if item == b' ' {
-            return i;
+fn chap10() {
+    print_chap(10, "Generic Types, Traits, and Lifetimes");
+/*
+    fn largest_f(list: &[i32]) -> i32 {
+        let mut largest = list[0];
+        for &item in list.iter() {
+            if item > largest {
+                largest = item;
+            }
+        }
+        largest
+    }
+*/
+    let number_list = vec![34, 50, 25, 100, 65];
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
         }
     }
-
-    s.len()
+//    let result = largest_f(&number_list);
+//    println!("The largest number is {} == {}", largest, result);
+    println!("The largest number is {}", largest);
 }
 
-fn first_word2(s: &str) -> &str {
-    let bytes = s.as_bytes();
+fn main() {
+    chap03();
+    chap04();
+    chap05();
+    chap06();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
+    chap08();
+    chap09();
+    chap10();
 }
-
